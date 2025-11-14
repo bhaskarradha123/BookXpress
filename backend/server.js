85d34406d514cbb4e64196671b9ad78b589e34df
Radha <122531010+bhaskarradha123@users.noreply.github.com>
2025-11-14 14:49:59 +0000

    refactor: improve code formatting and streamline MongoDB connection logic

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-const express=require('express'); 
-const mongoose=require('mongoose');
-const cors=require('cors');
-const env=require('dotenv'); 
-// const userRoutes=require('./routes/userRoute');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const env = require('dotenv');
 
+env.config();  // load .env
 
-const app=express();
-app.use(cors()); 
-app.use(express.json()); 
-// app.use('/', userRoutes); 
-mongoose.connect(env.config().parsed.MONGODB_URL, {
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   dbName: "bookXpress",
 })
 .then(() => {
-  app.listen(()=>{
-    console.log("Running");
-    
- });
+  console.log("MongoDB Connected");
+  
+  app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
+  });
 })
 .catch((error) => {
   console.error("MongoDB connection error:", error);
 });
-
-
-
