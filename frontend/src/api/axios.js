8c52bda86edf775f1b4747a14f20f2f0d5db648a
Radha <122531010+bhaskarradha123@users.noreply.github.com>
2025-11-15 09:46:22 +0000

    fix: update baseURL in axios instance to include '/api' path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:'https://bookxpress-t5vz.onrender.com',
+  baseURL:'https://bookxpress-t5vz.onrender.com/api',
 });
 
 // Attach token automatically
